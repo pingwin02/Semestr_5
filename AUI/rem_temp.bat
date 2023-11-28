25f3fe1f686e65f5a64db6355910a2aa2fe3f6e6
Damian Jankowski <108282259+pingwin02@users.noreply.github.com>
2023-11-28 13:44:11 +0100

    Update cleanup script in rem_temp.bat

--- AUI/rem_temp.bat
+++ AUI/rem_temp.bat
@@ -1,2 +1,2 @@
-FOR /d /r . %%d IN (.idea target) DO @IF EXIST "%%d" rd /s /q "%%d"
-pause
+FOR /r . %%d IN (.idea target node_modules .angular) DO @IF EXIST "%%d" rd /s /q "%%d"
+FOR /r . %%d IN (*.zip package-lock.json) DO @IF EXIST "%%d" del /s /q "%%d"
